2b67ace5c98331207f61923f546e2e38a64566e7
Chandler Ferry <chandlerferry@gmail.com>
2024-03-17 04:19:27 -0700

    feat: convertJavaEnumToTypeScriptInterfaceWithComments

---
+++ tools/scripts/runelite/createDts.spec.ts
@@ -0,0 +1,9 @@
+// TEST
+// const javaEnumString = `
+// public enum TestEnum {
+//     VALUE1,
+//     VALUE2,
+//     VALUE3
+// }
+// `;
+// console.log(convertJavaEnumToJSInterface(javaEnumString));
