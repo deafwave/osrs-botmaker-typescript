e66075534e820b24db2196f2b4e4f46d43b28401
Chandler Ferry <chandlerferry@gmail.com>
2024-03-18 13:32:48 -0700

    chore: init net.runelite.client

--- types/runelite/runelite.d.ts
+++ types/runelite/runelite.d.ts
@@ -1,9 +1,4 @@
 /// <reference path="./api/Client.d.ts" />
 
-declare namespace net.runelite {
-	export let client: any;
-}
-
-declare const client: net.runelite.api.Client;
-
 import client = net.runelite.api.Client;
+declare const client: net.runelite.api.Client;
