052725a8c16c12793026f82589736f0d36e88fc0
Chandler Ferry <chandlerferry@gmail.com>
2024-04-03 00:37:56 -0700

    fix: botmaker enumId override

--- rollup.cjs
+++ rollup.cjs
@@ -2,6 +2,7 @@ const { getBabelOutputPlugin } = require('@rollup/plugin-babel');
 const cleanup = require('rollup-plugin-cleanup');
 const alias = require('@rollup/plugin-alias');
 const npcIdMap = require('./types/runelite/api/NpcID.cjs');
+const enumIdMap = require('./types/runelite/api/EnumID.cjs');
 function removeExports() {
 	return {
 		name: 'remove-exports', // name of the plugin
@@ -50,12 +51,17 @@ function soxBugFixes() {
 	return {
 		name: 'sox-bugfix', // name of the plugin
 		renderChunk(code, _chunk, _options) {
-			const modifiedCode = code.replaceAll(
+			const modifiedNpcIds = code.replaceAll(
 				/net\.runelite\.api\.NpcID\.(.*?)([ ),;\]])/g,
 				(_, p1, p2) => `${npcIdMap[p1]}${p2}`,
 			);
+
+			const modifiedEnumIds = modifiedNpcIds.replaceAll(
+				/net\.runelite\.api\.EnumID\.(.*?)([ ),;\]])/g,
+				(_, p1, p2) => `${enumIdMap[p1]}${p2}`,
+			);
 			return {
-				code: modifiedCode,
+				code: modifiedEnumIds,
 				map: null,
 			};
 		},
