c2f3e81f5acc5bd8f28af24e96ff5eef9470556d
Chandler Ferry <chandlerferry@gmail.com>
2024-06-03 08:06:47 -0700

    chore: cleanup

--- types/runelite/client/ui/overlay/components/ProgressPieComponents.d.ts
+++ types/runelite/client/ui/overlay/components/ProgressPieComponents.d.ts
@@ -1,18 +1,3 @@
-<<<<<<< HEAD
-declare namespace net.runelite.client.ui.overlay.components {
-    
-    export class ProgressPieComponent {
-
-        setBorder (border:Color, size: number): void;
-        setBorderColor (borderColor: Color): void;
-        setDiameter (diameter: number): void;
-        setFill (fill: Color): void;
-        setPosition (position: net.runelite.api.Point): void;
-        setProgress (progress: number): void;
-        setStroke (stroke: Stroke): void;
-    }
-}
-=======
 /// <reference path="../../../../api/Point.d.ts" />
 /// <reference path="../../../../../java/index.d.ts" />
 declare namespace net.runelite.client.ui.overlay.components {
@@ -26,4 +11,3 @@ declare namespace net.runelite.client.ui.overlay.components {
 		setStroke(stroke: Stroke): void;
 	}
 }
->>>>>>> ca6675f395af90ad40e8632b45bb5c07766244cd
