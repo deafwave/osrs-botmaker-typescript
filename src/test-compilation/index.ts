08c2b0494557ff0ad4f7fec6c305073ee48be62c
Chandler Ferry <chandlerferry@gmail.com>
2025-09-04 00:47:27 -0700

    feat: allowJs

--- src/test-compilation/index.ts
+++ src/test-compilation/index.ts
@@ -1,4 +1,5 @@
 /// <reference types="@deafwave/osrs-botmaker-types" />
+import { exampleJavaScriptFunction } from './without-typescript.js';
 
 /* eslint-disable @typescript-eslint/no-unused-vars */
 
@@ -11,33 +12,34 @@
  *
  */
 
-export function onStart() {
+export function onStart(): void {
 	bot.printGameMessage('Executed JS onStart Method');
+	exampleJavaScriptFunction();
 }
 
-export function onGameTick() {
+export function onGameTick(): void {
 	//bot.printGameMessage('Executed JS onGameTick Method');
 }
 
-export function onNpcAnimationChanged(npc: net.runelite.api.Actor) {
+export function onNpcAnimationChanged(npc: net.runelite.api.Actor): void {
 	//bot.printGameMessage('Executed JS onNpcAnimationChanged Method ' + npc.getId());
 }
 
-export function onActorDeath(actor: net.runelite.api.Actor) {
+export function onActorDeath(actor: net.runelite.api.Actor): void {
 	//bot.printGameMessage('Executed JS onActorDeath Method');
 }
 
 export function onHitsplatApplied(
 	actor: net.runelite.api.Actor,
 	hitsplat: net.runelite.api.Hitsplat,
-) {
+): void {
 	//bot.printGameMessage('Executed JS onHitsplatApplied Method');
 }
 
 export function onInteractingChanged(
 	sourceActor: net.runelite.api.Actor,
 	targetActor: net.runelite.api.Actor,
-) {
+): void {
 	//bot.printGameMessage('Executed JS onInteractingChanged Method');
 }
 
@@ -45,6 +47,6 @@ export function onChatMessage(
 	type: net.runelite.api.ChatMessageType,
 	name: string,
 	message: string,
-) {
+): void {
 	//bot.printGameMessage('Executed JS onChatMessage Method: ' + name);
 }
---
+++ src/test-compilation/without-typescript.js
@@ -0,0 +1,3 @@
+export function exampleJavaScriptFunction() {
+	bot.printGameMessage('Hello from JavaScript!');
+}
