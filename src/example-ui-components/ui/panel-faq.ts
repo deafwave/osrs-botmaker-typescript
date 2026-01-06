8d8ff1a1f53be6471325223e9bd5137a4b550a1e
jbiskit <jakearnott99@gmail.com>
2026-01-07 08:44:56 +1100

    chore: fix faq spelling

--- src/example-ui-components/ui/panel-faq.ts
+++ src/example-ui-components/ui/panel-faq.ts
@@ -2,7 +2,7 @@ export const SWING_UI_SUMMARY = `Swing UI Summary
 
 To summarize the work we have done, we have created a Swing UI that allows you to create and manage your UI components.
 
-The helper functions are designed to be allow for simple UI element, while incorporating methods for caching values directly to the bmCache, without much additional effort.
+The helper functions are designed to allow for simple UI element, while incorporating methods for caching values directly to the bmCache, without much additional effort.
 
 The example panel is there to offer some insight in to how the Swing UI can be used, and is by no means a comprehensive example of the Swing UI.
 There are many additional changes available for you to leverage, and personalize based on your own design choices. This was done to offer a starting point for you to build upon, and is not intended to be a complete solution.
@@ -21,4 +21,6 @@ pnpm build
 Upon validation that the types work, I suggest you make a PR request to the Types repository to have the changes merged, for all to enjoy and benefit from.
 
 Once your changes are merged, you will be able to update the Types version to that on the Types repository and once again, running pnpm i to update the dependencies.
+
+-bik
 `;
